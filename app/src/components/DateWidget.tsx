24649d029b1323fade1c9d2cac2c0a1530369e97
Sammii <kellow.sammii@gmail.com>
2024-10-07 21:41:04 +0100

    fixing TS errors on nths function

--- app/src/components/DateWidget.tsx
+++ app/src/components/DateWidget.tsx
@@ -3,7 +3,7 @@ import { months } from "../../utils/months";
 export const DateWidget = () => {
   const getWrittenDate = () => {
     const date = new Date().toLocaleDateString();
-    const nth = n => n>3&&n<21?"th":n%10==1?"st":n%10==2?"nd":n%10==3?"rd":"th";
+    const nth = (n: number) => n>3&&n<21?"th":n%10==1?"st":n%10==2?"nd":n%10==3?"rd":"th";
     
     const dateArray = date.split("/");
     const writtenDate = `${parseInt(dateArray[1])}${nth(parseInt(dateArray[1]))} ${months[parseInt(dateArray[0]) - 1]}`;
