65cd9aba29e2c78e6e957047432d50afeb0e5688
Sammii <kellow.sammii@gmail.com>
2024-10-07 21:39:08 +0100

    creating DateWidget

---
+++ app/src/components/DateWidget.tsx
@@ -0,0 +1,17 @@
+import { months } from "../../utils/months";
+
+export const DateWidget = () => {
+  const getWrittenDate = () => {
+    const date = new Date().toLocaleDateString();
+    const nth = n => n>3&&n<21?"th":n%10==1?"st":n%10==2?"nd":n%10==3?"rd":"th";
+    
+    const dateArray = date.split("/");
+    const writtenDate = `${parseInt(dateArray[1])}${nth(parseInt(dateArray[1]))} ${months[parseInt(dateArray[0]) - 1]}`;
+    
+    return writtenDate;
+  }
+  
+  return (
+    <p className="w-full flex justify-center">{getWrittenDate()}</p>
+  )
+};
