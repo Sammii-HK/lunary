6ba9bdf4c609cbf8b98273697f3865370863afc3
Sammii <kellow.sammii@gmail.com>
2024-10-28 19:00:00 +0000

    refactoring DateWidget

--- app/src/components/DateWidget.tsx
+++ app/src/components/DateWidget.tsx
@@ -1,33 +1,36 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
-import { months } from "../../utils/months";
+import { useState } from "react";
 import { useAstronomyContext } from "@/context/AstronomyContext";
 
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import dayjs, { Dayjs } from "dayjs";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export const DateWidget = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [value, setValue] = useState<Dayjs | null>(dayjs());
-  // const [writtenDate, setWrittenDate] = useState(getWrittenDate(dayjs().toDate()));
-  const { setCurrentDate, currentDate, writtenDate } = useAstronomyContext();
-
-  console.log("currentDate", currentDate);
-  console.log("writtenDate", writtenDate);
+  const { writtenDate, setCurrentDateTime } = useAstronomyContext();
   
   return (
     <>
       <p className="w-full flex justify-center" onClick={() => setIsOpen(true)}>{writtenDate}</p>
       {isOpen && <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar value={value} onChange={(newValue) => {
-          setValue(newValue)
-          setCurrentDate(newValue.toDate())
-          // setWrittenDate(getWrittenDate(newValue.toDate()))
-          setIsOpen(false)
-        }} />
+        <ThemeProvider theme={darkTheme}>
+          <DateCalendar views={['day']} value={value} onChange={(newValue) => {
+            setValue(newValue)
+            setCurrentDateTime(newValue.toDate())
+            setIsOpen(false)
+          }} />
+        </ThemeProvider>
       </LocalizationProvider>
       }
     </>
