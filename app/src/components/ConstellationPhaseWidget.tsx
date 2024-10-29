b61eba585518dd165369ad2406c5aa1bf4dec7cf
Sammii <kellow.sammii@gmail.com>
2024-10-29 17:31:45 +0000

    refactoring ConstellationPhaseWidget

--- app/src/components/ConstellationPhaseWidget.tsx
+++ app/src/components/ConstellationPhaseWidget.tsx
@@ -2,30 +2,64 @@
 
 import { constellationItems, getIcon, } from "../../utils/zodiac/zodiac";
 import { useAstronomyContext } from "@/context/AstronomyContext";
-import { stringToCamelCase } from "../../utils/moon/moonPhases";
+import { MoonPhaseLabels, stringToCamelCase } from "../../utils/moon/moonPhases";
 import { constellationsMoonPhases } from "../../utils/moon/zodiacPhases";
 import { MoonPhase } from "../../utils/moon/moonPhases";
+// import { checkForEquinoxOrSolstice } from "../../utils/astrology/astrology";
+import { constellations } from "../../utils/constellations";
+
 
 export const ConstellationPhaseWidget = () => {
   const {currentMoonConstellation, currentMoonPhase} = useAstronomyContext()
 
   if (!currentMoonConstellation) return null;
 
-  const constellationItems = [ 'element', 'rulingPlanet', 'quality', 'symbol' ] as const;
-  const currentMoonConstellationName = currentMoonConstellation.name?.toLowerCase() as keyof typeof constellationsMoonPhases;
-  const currentConstellationMoonPhases = constellationsMoonPhases[currentMoonConstellationName]
-  const phaseConstellationInformation = currentConstellationMoonPhases[stringToCamelCase(currentMoonPhase) as MoonPhase];
-  
   return (
-    <>
-    <div className="flex mb-3 flex-wrap grid-cols-2 justify-between">
-      {constellationItems.map(item => (
-        <ConstellationItem key={item} item={item} constellation={currentMoonConstellation} />
-      ))}
-    </div>
-    <p className="mb-4 text-[12px]">{phaseConstellationInformation?.details}</p>
-  </>
-  )
+		<LoadedConstellationPhaseWidget
+			currentMoonConstellation={currentMoonConstellation}
+			currentMoonPhase={currentMoonPhase}
+		/>
+	);
+};
+
+export const LoadedConstellationPhaseWidget = ({
+	currentMoonConstellation,
+	currentMoonPhase,
+}: {
+	currentMoonConstellation: (typeof constellations)[keyof typeof constellations];
+	currentMoonPhase: MoonPhaseLabels;
+}) => {
+	const constellationItems = [
+		"element",
+		"rulingPlanet",
+		"quality",
+		"symbol",
+	] as const;
+	const currentMoonConstellationName =
+		currentMoonConstellation.name?.toLowerCase() as keyof typeof constellationsMoonPhases;
+	const currentConstellationMoonPhases =
+		constellationsMoonPhases[currentMoonConstellationName];
+	const phaseConstellationInformation =
+		currentConstellationMoonPhases[
+			stringToCamelCase(currentMoonPhase) as MoonPhase
+		];
+
+	return (
+		<>
+			<div className="flex mb-3 flex-wrap grid-cols-2 justify-between">
+				{constellationItems.map((item) => (
+					<ConstellationItem
+						key={item}
+						item={item}
+						constellation={currentMoonConstellation}
+					/>
+				))}
+			</div>
+			<p className="mb-4 text-[12px]">
+				{phaseConstellationInformation?.details}
+			</p>
+		</>
+	);
 };
 
 const ConstellationItem = ({
