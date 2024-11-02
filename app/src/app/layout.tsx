a6e7646ea24a10e54ff62d415362b09e04221235
Sammii <kellow.sammii@gmail.com>
2024-11-02 08:47:31 +0000

    add navbar to layout

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 import { getMoonSymbol } from '../../utils/moon/moonPhases';
+import { Navbar } from '@/components/Navbar';
 
 export const metadata: Metadata = {
   title: `${getMoonSymbol()} Lunary`,
@@ -18,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} w-full h-screen flex flex-col align-middle items-center`}>
+        <main className='flex flex-col h-full max-w-md w-auto items-center justify-between font-mono text-sm gap-4 overflow-auto p-4 align-self-middle justify-self-center'>
+          {children}
+        </main>
+        <Navbar />
+      </body>
     </html>
   );
 }
---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { BookMarked, Eclipse, Notebook, Sparkles } from 'lucide-react';
+import Link from 'next/link';
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="sticky bottom-0 w-full h-fit border-stone-800 border-t border-spacing-5 flex justify-center bg-stone-950 bg-neutral-950">
+        <ul className='flex justify-around my-1 pb-1 w-full lg:w-[50dvw]'>
+          <li>
+            <Link href="/" className="flex items-center columns-1 cursor-pointer p-2">
+              <Eclipse />
+            </Link>
+          </li>
+          <li>
+            <Link href="/tarot" className="flex items-center columns-1 cursor-pointer p-2">
+              <Sparkles />
+            </Link>
+          </li>
+          <li>
+            <Link href="/grimoire" className="flex items-center columns-1 cursor-pointer p-2">
+              <BookMarked />
+            </Link>
+          </li>
+          <li>
+            <Link href="/book-of-shadows" className="flex items-center columns-1 cursor-pointer p-2">
+              <Notebook />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
