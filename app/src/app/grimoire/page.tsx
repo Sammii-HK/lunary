045b7420ff52669dc96fcf52f3e93845670b33b5
Sammii <kellow.sammii@gmail.com>
2024-11-02 15:52:28 +0000

    refactoring grimoire

--- app/src/app/grimoire/page.tsx
+++ app/src/app/grimoire/page.tsx
@@ -1,52 +1,83 @@
 import Link from 'next/link';
 
+const grimoire: { [key: string]: { title: string; contents?: string[] } } = {
+  moon: {
+    title: 'Moon',
+    contents: [
+      'Phases',
+      'Full Moon Names',
+    ]
+  },
+  wheelOfTheYear: {
+    title: 'Wheel of the Year',
+    // contents: [
+    //   'sabbats',
+    // ]
+  },
+  astrology: {
+    title: 'Astrology',
+    contents: [
+      'Planets',
+      'Zodiac',
+    ]
+  },
+  tarot: {
+    title: 'Tarot',
+    contents: [
+      'Arcana',
+      'Spreads',
+    ]
+  },
+  runes: {
+    title: 'Runes',
+  },
+  chakras: {
+    title: 'Chakras',
+  },
+  crystals: {
+    title: 'Crystals',
+  },
+  correspondences: {
+    title: 'Correspondences',
+    contents: [
+      'Elements',
+      'Colors',
+      'Days',
+      'Deities',
+      'Flowers',
+      'Numbers',
+      'Wood',
+      'Herbs',
+      'Animals',
+    ]
+  },
+  practices: {
+    title: 'Practices',
+    contents: [
+      'Spells',
+      'Rituals',
+      'Recipes',
+      'Divination',
+    ]
+  },
+}
+
+const grimoireItems = Object.keys(grimoire);
+
 const Grimoire = () => {
   return (
     <div className="h-[91vh]">
       <h1 className="text-lg font-bold mb-4">Grimoire</h1>
       <p>Search...</p>
-      {/* <li>Table of Contents</li> */}
-      {/* <h2>Index</h2> */}
-      <ul>
-        {/* <li>Moon</li>  */}
-        <Link href="/grimoire/moon" className="font-bold mt-2">Moon</Link>
-        {/* // phases, full moon names */}
-            
-        <li>Wheel of the Year</li> 
-        {/* // sabbats */}
-        {/* <li>Sabbat</li> */}
-        {/* days of the week */}
-
-        <li>Astrology</li>
-        {/* <li>Zodiac</li>
-            <li>Tree Zodiac</li> */}
-
-        <li>Tarot</li>
-        <li>Runes</li>
-        <li>Chakras</li>
-        <li>Crystals</li>
-
-        <Link href="/grimoire/correspondences" className="font-bold mt-2">Correspondences</Link>
-        <ul>
-          <li>Elements</li>
-          <li>Colors</li>
-          <li>Days</li>
-          <li>Deities</li>
-          <li>FLowers</li>
-          <li>Numbers</li>
-          <li>Wood</li>
-          <li>Herbs</li>
-          <li>Animals</li>
-        </ul>
-
-        <Link href="/grimoire/practices" className="font-bold mt-2">Practices</Link>
-        <ul>
-          <li>Spells</li>
-          <li>Rituals</li>
-          <li>Recipes</li>
-          <li>Divination</li>
-
-        </ul>
+      <ul className='flex flex-col'>
+        {grimoireItems.map((item: string) => (
+          <>
+            <Link href={`/grimoire/${item}`} className="mt-3 mb-1 font-bold">{grimoire[item].title}</Link>
+            {grimoire[item].contents?.map((content: string) => (
+              <Link href={`/grimoire/${item}#${content.replaceAll(' ', '-').toLowerCase()}`} className="font-light mb-1 pl-3">{content}</Link>
+            ))}
+          </>
+        ))}
       </ul>
     </div>
   );
