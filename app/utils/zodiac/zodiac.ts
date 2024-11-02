56c42df6d57ae4d4180e9317b38d3775c1acfb65
Sammii <kellow.sammii@gmail.com>
2024-11-02 16:16:04 +0000

    zodiac file tidy

--- app/utils/zodiac/zodiac.ts
+++ app/utils/zodiac/zodiac.ts
@@ -1,7 +1,3 @@
-"use client"
-
-import { GeoVector, Body, Vector } from 'astronomy-engine';
-
 export const ZODIAC_SIGNS = [
   "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
   "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"
