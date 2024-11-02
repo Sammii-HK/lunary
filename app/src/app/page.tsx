3eaa30e33d76b68628695cf0dd7db575602eadaf
Sammii <kellow.sammii@gmail.com>
2024-11-02 08:49:25 +0000

    fix styling on app/page, refactor for new root Layout comment out horoscope widget

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,26 +3,19 @@ import { MoonWidget } from '../components/MoonWidget';
 import { AstronomyWidget } from '@/components/AstronomyWidget';
 import { DateWidget } from '@/components/DateWidget';
 import { AstronomyContextProvider } from '@/context/AstronomyContext';
-import { HoroscopeWidget } from '@/components/HoroscopeWidget';
-// import { Navbar } from '@/components/Navbar';
+// import { HoroscopeWidget } from '@/components/HoroscopeWidget';
 
 export default function Home() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-between gap-4 ">
-      {/* bg-stone-900 bg-neutral-950 */}
-      {/* // <div className="flex max-h-screen min-w-full flex-col z-10 max-w-md w-screen min-h-screen items-center justify-between font-mono text-sm gap-4 overflow-auto"> */}
-      {/* <div className="z-10 max-w-md w-screen min-h-screen items-center justify-between font-mono text-sm gap-4 overflow-auto"> */}
-      {/* <Layout> */}
+    <div className="flex h-fit-content w-full flex-col items-center justify-between gap-4 ">
       <AstronomyContextProvider>
         <DateWidget />
         <AstronomyWidget />
         <MoonWidget />
         <TarotWidget />
-        <HoroscopeWidget />
+        {/* <HoroscopeWidget /> */}
       </AstronomyContextProvider>
-      {/* </Layout>  */}
     </div>
-    // </div>
   );
 }
 
