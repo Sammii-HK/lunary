c22896ec204958ae0c82184b9e7ab9b9c70ad1d4
Sammii <kellow.sammii@gmail.com>
2024-11-03 12:17:02 +0000

    tarotSuits const

---
+++ app/src/constants/tarot.ts
@@ -0,0 +1,26 @@
+export const tarotSuits = {
+  wands: {
+    name: 'Wands',
+    element: 'Fire',
+    qualities: 'Creativity, action, ambition, growth',
+    mysticalProperties: 'Associated with the pursuit of goals, personal development, and spirituality. Represents enterprise and distinction in matters of career and personal projects.'
+  },
+  cups: {
+    name: 'Cups',
+    element: 'Water',
+    qualities: 'Emotions, relationships, feelings, intuition',
+    mysticalProperties: 'Focuses on emotional aspects of life, including love, friendship, and the subconscious. Symbolizes emotional intelligence, artistic endeavors, and relationships.'
+  },
+  swords: {
+    name: 'Swords',
+    element: 'Air',
+    qualities: 'Intellect, thought, justice, truth',
+    mysticalProperties: 'Deals with the realm of the mind and intellect. Associated with conflict, moral integrity, and decisions. Represents challenges and changes in one’s thoughts or beliefs.'
+  },
+  pentacles: {
+    name: 'Pentacles',
+    element: 'Earth',
+    qualities: 'Material aspects, prosperity, practicality, trust',
+    mysticalProperties: 'Linked to the material and physical world. Represents manifestations, financial situations, and aspects of wealth. Focuses on issues related to work, possessions, and health.'
+  }
+};
