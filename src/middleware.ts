867ff9ad84643c7a073dc236df7f95b19f937b5c
Sammii <kellow.sammii@gmail.com>
2025-11-14 15:54:13 +0000

    addming admin manifest to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -101,6 +101,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sw.js|manifest.json).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sw.js|manifest.json|admin-manifest.json|icons/pwa).*)',
   ],
 };
